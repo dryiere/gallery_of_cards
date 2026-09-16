ec25e36d2d7eef67b483ceb6f66cfc5dc8f82e7f
unknown <user@V09E.edu2.zsl.poznan.pl>
2026-09-16 10:31:47 +0200

    Navbar import

--- 'gallery_of_cards'/src/App.jsx
+++ 'gallery_of_cards'/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-
+import Navbar from './components/Navbar'
 import './App.css'
 
 function App() {
@@ -7,6 +7,10 @@ function App() {
 
   return (
     <>
+    <Navbar/>
+    <div className="container mt-4">
+      <h1>Galeria zdjęć</h1>
+    </div>
     </>
   )
 }
